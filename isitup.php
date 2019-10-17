424d7444c12c899fe5533b9c22467b6054da2df1
CackleClub <46990941+CackleClub@users.noreply.github.com>
2019-10-17 13:31:09 -0500

    Now works with post 443!

--- isitup.php
+++ isitup.php
@@ -1,20 +1,23 @@
 <?php
 $url = $_GET['i'];
-
+$url_sanitized = filter_var ( $url, FILTER_SANITIZE_STRING);
 
 if($url == null){
     echo " ";
 }
 else{
-    $fp = @fSockOpen($url,80,$errno,$errstr,1);
+    $fp = @fSockOpen($url_sanitized,80,$errno,$errstr,1);
+    $fpssl = @fSockOpen($url,443,$errno,$errstr,1);
        if(is_resource($fp)){
             fclose($fp); 
-            echo filter_var ( $url, FILTER_SANITIZE_STRING).' Is Up!'; 
+            echo $url_sanitized.' Is Up!'; 
         }
-    
-    else{
-        echo filter_var ( $url, FILTER_SANITIZE_STRING).' Is Down!'; 
-
-    }
+        elseif(is_resource($fpssl)) {
+            fclose($fp); 
+            echo $url_sanitized.' Is Up!';
+        }
+        else{
+        echo $url_sanitized.' Is Down!'; 
+        }
 }
 ?>
