08619daa69e1a9aeeaa9bb2875348e0699dfc9ab
CackleClub <46990941+CackleClub@users.noreply.github.com>
2019-10-17 12:05:00 -0500

    "streamlined" if statements

--- isitup.php
+++ isitup.php
@@ -9,12 +9,12 @@ else{
 $checkCount = 0;
     $fp = @fSockOpen($url,80,$errno,$errstr,1);
        if(is_resource($fp)){
-        if($fp) { 
+        
             $status=0; 
             fclose($fp); 
             echo filter_var ( $url, FILTER_SANITIZE_STRING).' Is Up!'; 
         }
-    }
+    
     else{
         echo filter_var ( $url, FILTER_SANITIZE_STRING).' Is Down!'; 
 
